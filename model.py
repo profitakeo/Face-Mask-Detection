f1a55d50ec6929cc1d18137ef926289a4a962962
proficles <66627355+proficles@users.noreply.github.com>
2020-12-16 15:07:16 +1100

    update

--- model.py
+++ model.py
@@ -1,29 +1,85 @@
-# Face Mask Detection
+# Importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import os
+from bs4 import BeautifulSoup
 
-class NetConv(nn.Module):
-    def __init__(self):
-        super(NetConv, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+# Loading data
+img_folder = "C:/Users/profi/Downloads/Face_Mask_Dataset_(from Kaggle)/images"
+annot_folder = "C:/Users/profi/Downloads/Face_Mask_Dataset_(from Kaggle)/annotations"
 
-        self.fc1 = nn.Linear(128, 3) # 128 might need to be changed
+# Extracting image name and class from xml file
+desc = []
+for dirname, _, filenames in os.walk(annot_folder):
+    for filename in filenames:
+        desc.append(os.path.join(dirname, filename))
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2)
+img_name,label = [],[]
 
-        x = x.view(-1, 128) # 128 might need to be changed
-        x = F.relu(self.fc1(x))
+for d in desc:
+    content = []
+    n = []
 
-        x = F.softmax(x, dim=1)
+    with open(d, "r") as file:
+        content = file.readlines()
+    content = "".join(content)
+    soup = BeautifulSoup(content,"html.parser")
+    file_name = soup.filename.string
+    name_tags = soup.find_all("name")
+    
 
-        return x
+    for t in name_tags:
+        n.append(t.get_text())
+        
+    # selecting tag with maximum occurence in an image (If it has multiple tags)
+    name = max(set(n), key = n.count)
+  
+    img_name.append(file_name)
+    label.append(name)
+
+img_name
+
+# One Hot Encoding label data
+labels = pd.get_dummies(label)
+print(labels.head())
+
+# Our target classes
+classes = list(labels.columns)
+print(classes)
+
+# Loading Images and converting them to pixel array
+data, target = [],[]
+img_h, img_w = 256, 256
+
+for i in range(len(img_name)):
+    name = img_name[i]
+    path = img_folder + name
+    
+    image = load_img(path, target_size = (img_h, img_w))
+    image = img_to_array(image)
+    data.append(image)
+    target.append(tuple(labels.iloc[i,:]))
+
+# Converting list to array
+data=np.array(data,dtype="float32")/255.0
+target=np.array(target,dtype="float32")
+
+# Visualizing few images randomly
+plt.figure(figsize=(10, 10))
+for i,j in enumerate(np.random.randint(1, 500, 9, dtype=int)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(data[j])
+    plt.title(label[j])
+    plt.axis("off")
+
+# Shape of data and target
+data.shape, target.shape
+
+# Splitting into train and test data
+train_img,test_img,y_train,y_test=train_test_split(data,target,test_size=0.2,random_state=20)
+
+print("Train shapes : ",(train_img.shape, y_train.shape))
+print("Test shapes : ",(test_img.shape, y_test.shape))
