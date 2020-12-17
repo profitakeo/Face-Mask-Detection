40977c7761253d489eaf988c4542a94c43778029
khakuzz <38999102+khakuzz@users.noreply.github.com>
2020-12-17 17:33:24 +1100

    need to finish training

--- training.py
+++ training.py
@@ -1,12 +1,17 @@
 # PREPROCESSING
 
 # Importing libraries
+import torch
+import torch.nn as nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import cv2
 import model
+import argparse
+import time
+
 
 # import tensorflow as tf
 # from keras.preprocessing.image import img_to_array,load_img
@@ -19,6 +24,14 @@ from bs4 import BeautifulSoup
 # Loading data
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no_cuda',action='store_true',default=False,help='disables CUDA')
+    args = parser.parse_args()
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device('cuda' if use_cuda else 'cpu')
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
     img_folder = "C:/Users/Harry/Desktop/Projects/Face-Mask-Detection/images"
     annot_folder = "C:/Users/Harry/Desktop/Projects/Face-Mask-Detection/annotations"
@@ -109,28 +122,122 @@ def main():
     print("Train shapes : ",(train_img.shape, y_train.shape))
     print("Test shapes : ",(test_img.shape, y_test.shape))
 
+    train(model, optimiser, nn.CrossEntropyLoss(), train_img, y_train, model.epochs, "cuda")
 
-    # for epoch in range(2):  # loop over the dataset multiple times
+    return
 
-    #     running_loss = 0.0
-    #     for i, data in enumerate(trainloader, 0): # where trainloader = torch.utils.data.DataLoader(...)
-    #         # get the inputs; data is a list of [inputs, labels]
-    #         inputs, labels = data
+def train(model, optimizer, loss_fn, train_dl, val_dl, epochs=20, device='cuda'):
+    '''
+    Runs training loop for classification problems. Returns Keras-style
+    per-epoch history of loss and accuracy over training and validation data.
 
-    #         # zero the parameter gradients
-    #         optimizer.zero_grad()
+    Parameters
+    ----------
+    model : nn.Module
+        Neural network model
+    optimizer : torch.optim.Optimizer
+        Search space optimizer (e.g. Adam)
+    loss_fn :
+        Loss function (e.g. nn.CrossEntropyLoss())
+    train_dl : 
+        Iterable dataloader for training data.
+    val_dl :
+        Iterable dataloader for validation data.
+    epochs : int
+        Number of epochs to run
+    device : string
+        Specifies 'cuda' or 'cpu'
 
-    #         # forward + backward + optimize
-    #         outputs = net(inputs)
-    #         loss = criterion(outputs, labels) # Classification Cross-Entropy loss 
-    #         loss.backward()
-    #         optimizer.step() # SGD with momentum optimizer
+    Returns
+    -------
+    Dictionary
+        Similar to Keras' fit(), the output dictionary contains per-epoch
+        history of training loss, training accuracy, validation loss, and
+        validation accuracy.
+    '''
 
-    #         # print statistics
-    #         running_loss += loss.item()
-    #         if i % 2000 == 1999:    # print every 2000 mini-batches
-    #             print('[%d, %5d] loss: %.3f' %
-    #                 (epoch + 1, i + 1, running_loss / 2000))
-    #             running_loss = 0.0
+    print('train() called: model=%s, opt=%s(lr=%f), epochs=%d, device=%s\n' % \
+          (type(model).__name__, type(optimizer).__name__,
+           optimizer.param_groups[0]['lr'], epochs, device))
+
+    history = {} # Collects per-epoch loss and acc like Keras' fit().
+    history['loss'] = []
+    history['val_loss'] = []
+    history['acc'] = []
+    history['val_acc'] = []
+
+    start_time_sec = time.time()
+
+    for epoch in range(epochs):
+
+        # --- TRAIN AND EVALUATE ON TRAINING SET -----------------------------
+        model.train()
+        train_loss         = 0.0
+        num_train_correct  = 0
+        num_train_examples = 0
+
+        for batch in train_dl:
+
+            optimizer.zero_grad()
+
+            x    = batch[0].to(device)
+            y    = batch[1].to(device)
+            yhat = model(x)
+            loss = loss_fn(yhat, y)
+
+            loss.backward()
+            optimizer.step()
+
+            train_loss         += loss.data.item() * x.size(0)
+            num_train_correct  += (torch.max(yhat, 1)[1] == y).sum().item()
+            num_train_examples += x.shape[0]
+
+        train_acc   = num_train_correct / num_train_examples
+        train_loss  = train_loss / len(train_dl.dataset)
+
+
+        # --- EVALUATE ON VALIDATION SET -------------------------------------
+        model.eval()
+        val_loss       = 0.0
+        num_val_correct  = 0
+        num_val_examples = 0
+
+        for batch in val_dl:
+
+            x    = batch[0].to(device)
+            y    = batch[1].to(device)
+            yhat = model(x)
+            loss = loss_fn(yhat, y)
+
+            val_loss         += loss.data.item() * x.size(0)
+            num_val_correct  += (torch.max(yhat, 1)[1] == y).sum().item()
+            num_val_examples += y.shape[0]
+
+        val_acc  = num_val_correct / num_val_examples
+        val_loss = val_loss / len(val_dl.dataset)
+
+
+        print('Epoch %3d/%3d, train loss: %5.2f, train acc: %5.2f, val loss: %5.2f, val acc: %5.2f' % \
+              (epoch+1, epochs, train_loss, train_acc, val_loss, val_acc))
+
+        history['loss'].append(train_loss)
+        history['val_loss'].append(val_loss)
+        history['acc'].append(train_acc)
+        history['val_acc'].append(val_acc)
+
+    # END OF TRAINING LOOP
+
+
+    end_time_sec       = time.time()
+    total_time_sec     = end_time_sec - start_time_sec
+    time_per_epoch_sec = total_time_sec / epochs
+    print()
+    print('Time total:     %5.2f sec' % (total_time_sec))
+    print('Time per epoch: %5.2f sec' % (time_per_epoch_sec))
+
+    return history
 
     print('Finished Training')
+
+if __name__ == '__main__':
+    main()
